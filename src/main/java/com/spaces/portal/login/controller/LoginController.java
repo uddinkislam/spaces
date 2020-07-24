4cec7c63d075c1140e07798a74028abd3d02325f
Islamuddin Noor <islamuddinnoor@Islamuddins-MBP.lan>
2020-07-24 11:36:27 -0500

    modified controller to handle post call and exceptions

--- src/main/java/com/spaces/portal/login/controller/LoginController.java
+++ src/main/java/com/spaces/portal/login/controller/LoginController.java
@@ -2,29 +2,33 @@
 package com.spaces.portal.login.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spaces.portal.login.repository.UserRepository;
 import com.spaces.portal.login.request.CreateUser;
-import com.spaces.portal.login.response.LoginResponse;
 
 @RestController
 public class LoginController {
 	
 	@Autowired
 	private UserRepository userData;
-	private String name;
 
-	@RequestMapping("/createUser")
+	@RequestMapping(value = "/createUser", method = RequestMethod.POST)
 	public String createUser(@RequestBody CreateUser user) {
+		String res;
+		try {
+			userData.save(user);
+			res = "success! user has been created";
+			
+		} catch (Exception e) {
+			res = "something went wrong! " + e.getMessage();
+			
+		}		
+		return res;
 		
-
-		userData.save(user);
-
-		return "success";
 
 	}
 
